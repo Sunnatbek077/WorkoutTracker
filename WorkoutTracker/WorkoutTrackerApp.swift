42722f39494f22943c954dc59df90d15c31fc9b8
Sunnatbek077 <sunnatbekabdunabiyev@icloud.com>
2026-03-01 21:39:51 +0500

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Sunnatbek on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
